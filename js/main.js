564b052639628ce8eae68602c92c21d1741f44bd
arthursiq5 <arthur.siq5@gmail.com>
2024-02-17 12:25:12 -0300

    Melhorado jogo

--- js/main.js
+++ js/main.js
@@ -1,59 +1,57 @@
-const emojis = [
-    "🐱",
-    "🐱",
-    "🐼",
-    "🐼",
-    "🦊",
-    "🦊",
-    "🐶",
-    "🐶",
-    "🦬",
-    "🦬",
-    "🦓",
-    "🦓",
-    "🐖",
-    "🐖",
-    "🐯",
-    "🐯",
-]
-let openCards = []
-let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1))
-for (let i = 0; i < emojis.length; i++) {
-
-    let box = document.createElement("div");
-    box.className = "item";
-    box.innerHTML = shuffleEmojis[i];
-    box.onclick = handleClick;
-    document.querySelector(".game").appendChild(box);
-
-}
-
-function handleClick() {
-    if (openCards.length < 2) {
-        this.classList.add("boxOpen");
-        openCards.push(this);
+(function() {
+    const emojis = [
+        "🐱",
+        "🐱",
+        "🐼",
+        "🐼",
+        "🦊",
+        "🦊",
+        "🐶",
+        "🐶",
+        "🦬",
+        "🦬",
+        "🦓",
+        "🦓",
+        "🐖",
+        "🐖",
+        "🐯",
+        "🐯",
+    ];
+    let openCards = [];
+    (function() {
+        let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+        const tabuleiroHtml = shuffleEmojis.map(emoji => `<div class="item">${emoji}</div>`)
+        document.getElementById('game').innerHTML = tabuleiroHtml.join('')
+        document.querySelectorAll('.item').forEach((item) => item.addEventListener('click', handleClick))
+    })();
+    
+    function handleClick() {
+        if (openCards.length < 2) {
+            this.classList.add("boxOpen");
+            openCards.push(this);
+        }
+    
+        if (openCards.length == 2) {
+            setTimeout(checkMatch, 500);
+        }
     }
-
-    if (openCards.length == 2) {
-        setTimeout(checkMatch, 500);
+    
+    function checkVictory() {
+        if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+            alert("Você venceu !");
+        }
     }
-}
-
-function checkMatch() {
-    console.log(openCards)
-
-    if (openCards[0].innerHTML === openCards[1].innerHTML) {
-
-        openCards[0].classList.add("boxMatch");
-        openCards[1].classList.add("boxMatch");
-    } else {
+    
+    function checkMatch() {
+        if (openCards[0].innerHTML === openCards[1].innerHTML) {
+            openCards[0].classList.add("boxMatch");
+            openCards[1].classList.add("boxMatch");
+            checkVictory()
+            openCards = [];
+            return;
+        }
         openCards[0].classList.remove("boxOpen");
         openCards[1].classList.remove("boxOpen");
+        openCards = [];
     }
-    openCards = [];
-    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
-
-        alert("Você venceu !");
-
-    }
-}
+})()
