771ffa46c98214be0f79242b43446c44010b1e66
arthursiq5 <arthur.siq5@gmail.com>
2024-02-17 11:57:23 -0300

    Adicionada viragem de cartas

--- js/main.js
+++ js/main.js
@@ -23,6 +23,21 @@ for (let i = 0; i < emojis.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
 
+}
+
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+        return;
+    }
+
+    if (openCards.length == 2) {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+        openCards = []
+    }
 }
